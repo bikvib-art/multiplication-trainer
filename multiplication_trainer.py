9e6d9483e6e16f9a162e856a4c4e6db12f509c12
bikvib-art <bikvib@gmail.com>
2025-09-29 10:55:23 +0300

    Update multiplication_trainer.py

--- multiplication_trainer.py
+++ multiplication_trainer.py
@@ -1,39 +1,58 @@
 import streamlit as st
-import random
+
+st.set_page_config(
+    page_title="Тренажер умножения",
+    page_icon="🧮",
+)
+
+st.title("✅ Тестовое приложение")
+st.success("Streamlit работает корректно!")
+st.write("Если вы видите это сообщение, значит базовый функционал работает.")
+
+if st.button("Проверить генерацию чисел"):
+    import random
+    number = random.randint(1, 10)
+    st.write(f"Случайное число: {number}")
 
 
-def check_answer(expression, correct_answer):
-  print(f'{expression} = ', end='')
-  user_answer = int(input())
-  while user_answer != correct_answer:
-    print('К сожалению, ты ошибся! Попробуй еще раз')
-    print(f'{expression} = ', end='')
-    user_answer = int(input())
-  print('Правильно!')
+
+#import streamlit as st
+#import random
 
 
-def game():
-  for try_count in range(20):
-    operations = ['умножение', 'деление']
-    operation = random.choice(operations)
-    if operation == 'умножение':
-      first_number = random.randint(0, 10)
-      second_number = random.randint(0, 10)
-      check_answer(f'{first_number} * {second_number}',
+#def check_answer(expression, correct_answer):
+  #print(f'{expression} = ', end='')
+  #user_answer = int(input())
+  #while user_answer != correct_answer:
+    #print('К сожалению, ты ошибся! Попробуй еще раз')
+    #print(f'{expression} = ', end='')
+    #user_answer = int(input())
+  #print('Правильно!')
+
+
+#def game():
+  #for try_count in range(20):
+    #operations = ['умножение', 'деление']
+    #operation = random.choice(operations)
+    #if operation == 'умножение':
+      #first_number = random.randint(0, 10)
+      #second_number = random.randint(0, 10)
+      #check_answer(f'{first_number} * {second_number}',
                    first_number * second_number)
-    elif operation == 'деление':
-      first_number = random.randint(0, 10)
-      second_number = random.randint(1, 10)
-      first_number *= second_number
-      check_answer(f'{first_number} : {second_number}',
+    #elif operation == 'деление':
+      #first_number = random.randint(0, 10)
+      #second_number = random.randint(1, 10)
+      #first_number *= second_number
+      #check_answer(f'{first_number} : {second_number}',
                    first_number / second_number)
-  print('Ты отлично справляешься!')
+  #print('Ты отлично справляешься!')
 
 
-print('Добро пожаловать в тренажер по умножению и делению!')
-start = input('Хочешь начать тренировку? Да - нажми 1, Нет - нажми 2')
-while start == '1':
-  game()
-  start = input('Хочешь продолжить? Да - нажми 1, Нет - нажми 2')
-else:
-  print('Ты молодец! Спасибо, что тренируешься регулярно :)')
+#print('Добро пожаловать в тренажер по умножению и делению!')
+#start = input('Хочешь начать тренировку? Да - нажми 1, Нет - нажми 2')
+#while start == '1':
+  #game()
+  #start = input('Хочешь продолжить? Да - нажми 1, Нет - нажми 2')
+#else:
+  #print('Ты молодец! Спасибо, что тренируешься регулярно :)')
+
